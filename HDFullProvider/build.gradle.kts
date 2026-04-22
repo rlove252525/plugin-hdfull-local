e5172ec2683153ec2fff795b9e2a759b2b589da1
rlove252525 <rlove20251@gmail.com>
2026-04-23 01:37:20 +0200

    Update build.gradle.kts

--- HDFullProvider/build.gradle.kts
+++ HDFullProvider/build.gradle.kts
@@ -1,3 +1,32 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.stormunblessed"
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    compileOnly("com.lagradost.cloudstream3:cloudstream:3.5.0") // Asegura versión reciente
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
+}
+
 version = 4
 
 cloudstream {
